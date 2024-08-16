1c60ec49d11770bb0247f3ac159b2b8c9358aca1
gourav <gaurav.mishra@kogo.ai>
2024-08-16 23:39:46 +0530

    F

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,17 +1,18 @@
-const mongoose = require("mongoose");
-const { ATLAS_DB_URL, NODE_ENV } = require("./server.config");
+const mongoose = require('mongoose');
+const { ATLAS_DB_URL, NODE_ENV } = require('./server.config');
+
 
 async function connectToDB() {
-  try {
-    if (NODE_ENV == "development") {
-      await mongoose.connect(ATLAS_DB_URL);
-    } else if (NODE_ENV == "prod") {
-      // await mongoose.connect(ATLAS_DB_URL);
+
+    try {
+        if(NODE_ENV == "development") {
+            await mongoose.connect(ATLAS_DB_URL);
+        } 
+    } catch(error) {
+        console.log('Unable to connect to the DB server');
+        console.log(error);
     }
-  } catch (error) {
-    console.log("Unable to connect to the DB server");
-    console.log(error);
-  }
+
 }
 
 module.exports = connectToDB;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const { PORT } = require("./config/server.config");
-// const apiRouter = require("./routes");
-// const errorHandler = require("./utils/errorHandler");
-// const connectToDB = require("./config/db.config");
+const { PORT } = require('./config/server.config');
+const apiRouter = require('./routes');
+const errorHandler = require('./utils/errorHandler');
+const connectToDB = require('./config/db.config');
+
 
 const app = express();
 
@@ -12,22 +13,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 
-// If any request comes and route starts with /api, we map it to apiRouter
-// app.use("/api", apiRouter);
 
-app.get("/ping", (req, res) => {
-  return res.json({ message: "Problem Service is alive" });
+// If any request comes and route starts with /api, we map it to apiRouter
+app.use('/api', apiRouter);
+
+
+app.get('/ping', (req, res) => {
+    return res.json({message: 'Problem Service is alive'});
 });
 
 // last middleware if any error comes
-// app.use(errorHandler);
+app.use(errorHandler);
 
 app.listen(PORT, async () => {
-  console.log(`Server started at PORT: ${PORT}`);
-  // await connectToDB();
-//   const Cat = mongoose.model('Cat', { name: String });
-
-// const kitty = new Cat({ name: 'Zildjian' });
-// kitty.save().then(() => console.log('meow'));
-  console.log("Successfully connected to db");
+    console.log(`Server started at PORT: ${PORT}`);
+    await connectToDB();
+    console.log("Successfully connected to db");
 });
